e0df92e9f675319ed58856175cc4c78e0997431e
Italo Valente <italo.se7en@gmail.com>
2025-06-07 00:24:01 -0300

    Update cadcliente.php

--- view/cadcliente.php
+++ view/cadcliente.php
@@ -6,16 +6,23 @@
     <title>Sistema de Gestão</title>
 </head>
 <body>
-    <h1> Sistema de Gestão  </h1>
-    </hr>
+    <h1>Sistema de Gestão</h1>
+    <hr/>
     <form action="../model/inserircliente.php" method="POST">
-        nome:<br/>
-        <input type="text" nome="cxnome"/><br/>
+        Nome:<br/>
+        <input type="text" name="cxnome"/><br/>
         E-mail:<br/>
-        <input type="text" nome="cxemail"/><br/>
+        <input type="text" name="cxemail"/><br/>
         Idade:<br/>
-        <input type="text" nome="cxidade"/><br/>
+        <input type="text" name="cxidade"/><br/>
         <input type="submit" value="Gravar">
-        
+    </form>
+    <hr/>
+    <form action="../model/pesquisanome.php" method="POST">
+    Digite o nome completo:<br/>
+    <input type="text" name="cxpesquisa"/>
+    <input type="submit" value="Pesquisar">
+    </from>  
+    
 </body>
 </html>
